e45978a9ca0b10e7cefa6348079b6661d41f84a5
andrew.markel <andrew.markel@MU-C1-01.mullen.pvt.k12.co.us>
2021-10-05 11:53:27 -0600

    Start at MovePiece line 115

--- Chess/src/MovePiece.java
+++ Chess/src/MovePiece.java
@@ -19,14 +19,14 @@ public class MovePiece {
 		}
 		
 		
-		System.out.println("Enter the space to move your " + PrintBoard.board[fromCoordinates[0]][fromCoordinates[1]] + " to.");
-		//toCoordinates;
 		do {
+			System.out.println("Enter the space to move your " + PrintBoard.board[fromCoordinates[0]][fromCoordinates[1]] + " to.");
 			toCoordinates = Input.input();
-			PrintBoard.board[toCoordinates[0]][toCoordinates[1]] = PrintBoard.board[fromCoordinates[0]][fromCoordinates[1]];
-			PrintBoard.board[fromCoordinates[0]][fromCoordinates[1]] = "  ";
 		}
-		while (!moveToChecks(toCoordinates[0], toCoordinates[1]));
+		while (moveToChecks(fromCoordinates[0], fromCoordinates[1]));
+		
+		PrintBoard.board[toCoordinates[0]][toCoordinates[1]] = PrintBoard.board[fromCoordinates[0]][fromCoordinates[1]];
+		PrintBoard.board[fromCoordinates[0]][fromCoordinates[1]] = "  ";
 	}
 	
 	public static void black() {
@@ -39,50 +39,55 @@ public class MovePiece {
 		
 		
 		
-		System.out.println("Enter the space to move your " + PrintBoard.board[fromCoordinates[0]][fromCoordinates[1]] + " to.");
-		//int[] toCoordinates;
 		do {
+			System.out.println("Enter the space to move your " + PrintBoard.board[fromCoordinates[0]][fromCoordinates[1]] + " to.");
 			toCoordinates = Input.input();
-			PrintBoard.board[toCoordinates[0]][toCoordinates[1]] = PrintBoard.board[fromCoordinates[0]][fromCoordinates[1]];
-			PrintBoard.board[fromCoordinates[0]][fromCoordinates[1]] = "  ";
 		}
-		while (!moveToChecks(toCoordinates[0], toCoordinates[1]));
+		while (moveToChecks(fromCoordinates[0],fromCoordinates[1]));
+		
+		PrintBoard.board[toCoordinates[0]][toCoordinates[1]] = PrintBoard.board[fromCoordinates[0]][fromCoordinates[1]];
+		PrintBoard.board[fromCoordinates[0]][fromCoordinates[1]] = "  ";
 	}
 	
 	public static void fillPiecesArray() {
-		pieces.add(new Piece(ANSI_TEXT+"wp"+ANSI_RESET,new int[] {6,0},0));
-//		pieces.add(ANSI_TEXT+"wp"+ANSI_RESET,6,1,0);
-//		pieces.add(ANSI_TEXT+"wp"+ANSI_RESET,6,2,0);
-//		pieces.add(ANSI_TEXT+"wp"+ANSI_RESET,6,3,0);
-//		pieces.add(ANSI_TEXT+"wp"+ANSI_RESET,6,4,0);
-//		pieces.add(ANSI_TEXT+"wp"+ANSI_RESET,6,5,0);
-//		pieces.add(ANSI_TEXT+"wp"+ANSI_RESET,6,6,0);
-//		pieces.add(ANSI_TEXT+"wp"+ANSI_RESET,6,7,0);
-//		pieces.add(ANSI_TEXT+"wr"+ANSI_RESET,7,0,0);
-//		pieces.add(ANSI_TEXT+"wn"+ANSI_RESET,7,1,0);
-//		pieces.add(ANSI_TEXT+"wb"+ANSI_RESET,7,2,0);
-//		pieces.add(ANSI_TEXT+"wk"+ANSI_RESET,7,3,0);
-//		pieces.add(ANSI_TEXT+"wq"+ANSI_RESET,7,4,0);
-//		pieces.add(ANSI_TEXT+"wb"+ANSI_RESET,7,5,0);
-//		pieces.add(ANSI_TEXT+"wn"+ANSI_RESET,7,6,0);
-//		pieces.add(ANSI_TEXT+"wr"+ANSI_RESET,7,7,0);
-//		pieces.add(ANSI_TEXT+"br"+ANSI_RESET,0,0,0);
-//		pieces.add(ANSI_TEXT+"bn"+ANSI_RESET,0,1,0);
-//		pieces.add(ANSI_TEXT+"bb"+ANSI_RESET,0,2,0);
-//		pieces.add(ANSI_TEXT+"bk"+ANSI_RESET,0,3,0);
-//		pieces.add(ANSI_TEXT+"bq"+ANSI_RESET,0,4,0);
-//		pieces.add(ANSI_TEXT+"bb"+ANSI_RESET,0,5,0);
-//		pieces.add(ANSI_TEXT+"bn"+ANSI_RESET,0,6,0);
-//		pieces.add(ANSI_TEXT+"br"+ANSI_RESET,0,7,0);
-//		pieces.add(ANSI_TEXT+"bp"+ANSI_RESET,1,0,0);
-//		pieces.add(ANSI_TEXT+"bp"+ANSI_RESET,1,1,0);
-//		pieces.add(ANSI_TEXT+"bp"+ANSI_RESET,1,2,0);
-//		pieces.add(ANSI_TEXT+"bp"+ANSI_RESET,1,3,0);
-//		pieces.add(ANSI_TEXT+"bp"+ANSI_RESET,1,4,0);
-//		pieces.add(ANSI_TEXT+"bp"+ANSI_RESET,1,5,0);
-//		pieces.add(ANSI_TEXT+"bp"+ANSI_RESET,1,6,0);
-//		pieces.add(ANSI_TEXT+"bp"+ANSI_RESET,1,7,0);
-		System.out.println(pieces.get(0).getLocation());
+		pieces.add(new Piece(ANSI_TEXT+"wp"+ANSI_RESET,new int[] {6,0},1));
+		pieces.add(new Piece(ANSI_TEXT+"wp"+ANSI_RESET,new int[] {6,1},0));
+		pieces.add(new Piece(ANSI_TEXT+"wp"+ANSI_RESET,new int[] {6,2},0));
+		pieces.add(new Piece(ANSI_TEXT+"wp"+ANSI_RESET,new int[] {6,3},0));
+		pieces.add(new Piece(ANSI_TEXT+"wp"+ANSI_RESET,new int[] {6,4},0));
+		pieces.add(new Piece(ANSI_TEXT+"wp"+ANSI_RESET,new int[] {6,5},0));
+		pieces.add(new Piece(ANSI_TEXT+"wp"+ANSI_RESET,new int[] {6,6},0));
+		pieces.add(new Piece(ANSI_TEXT+"wp"+ANSI_RESET,new int[] {6,7},0));
+		pieces.add(new Piece(ANSI_TEXT+"wr"+ANSI_RESET,new int[] {7,0},0));
+		pieces.add(new Piece(ANSI_TEXT+"wn"+ANSI_RESET,new int[] {7,1},0));
+		pieces.add(new Piece(ANSI_TEXT+"wb"+ANSI_RESET,new int[] {7,2},0));
+		pieces.add(new Piece(ANSI_TEXT+"wk"+ANSI_RESET,new int[] {7,3},0));
+		pieces.add(new Piece(ANSI_TEXT+"wq"+ANSI_RESET,new int[] {7,4},0));
+		pieces.add(new Piece(ANSI_TEXT+"wb"+ANSI_RESET,new int[] {7,5},0));
+		pieces.add(new Piece(ANSI_TEXT+"wn"+ANSI_RESET,new int[] {7,6},0));
+		pieces.add(new Piece(ANSI_TEXT+"wr"+ANSI_RESET,new int[] {7,7},0));
+		pieces.add(new Piece(ANSI_TEXT+"bp"+ANSI_RESET,new int[] {1,0},0));
+		pieces.add(new Piece(ANSI_TEXT+"bp"+ANSI_RESET,new int[] {1,1},0));
+		pieces.add(new Piece(ANSI_TEXT+"bp"+ANSI_RESET,new int[] {1,2},0));
+		pieces.add(new Piece(ANSI_TEXT+"bp"+ANSI_RESET,new int[] {1,3},0));
+		pieces.add(new Piece(ANSI_TEXT+"bp"+ANSI_RESET,new int[] {1,4},0));
+		pieces.add(new Piece(ANSI_TEXT+"bp"+ANSI_RESET,new int[] {1,5},0));
+		pieces.add(new Piece(ANSI_TEXT+"bp"+ANSI_RESET,new int[] {1,6},0));
+		pieces.add(new Piece(ANSI_TEXT+"bp"+ANSI_RESET,new int[] {1,7},0));
+		pieces.add(new Piece(ANSI_TEXT+"br"+ANSI_RESET,new int[] {0,0},0));
+		pieces.add(new Piece(ANSI_TEXT+"bn"+ANSI_RESET,new int[] {0,1},0));
+		pieces.add(new Piece(ANSI_TEXT+"bb"+ANSI_RESET,new int[] {0,2},0));
+		pieces.add(new Piece(ANSI_TEXT+"bk"+ANSI_RESET,new int[] {0,3},0));
+		pieces.add(new Piece(ANSI_TEXT+"bq"+ANSI_RESET,new int[] {0,4},0));
+		pieces.add(new Piece(ANSI_TEXT+"bb"+ANSI_RESET,new int[] {0,5},0));
+		pieces.add(new Piece(ANSI_TEXT+"bn"+ANSI_RESET,new int[] {0,6},0));
+		pieces.add(new Piece(ANSI_TEXT+"br"+ANSI_RESET,new int[] {0,7},0));
+		
+		
+//		for (int i = 0; i < pieces.size(); i ++) {
+//			System.out.println("location:" + pieces.get(i).getLocation()[0] + pieces.get(i).getLocation()[1]);
+//			System.out.println("piece:" + PrintBoard.board[pieces.get(i).getLocation()[0]][pieces.get(i).getLocation()[1]]);
+//		}
 	}
 	
 	private static boolean moveToChecks(int row, int col) {
@@ -90,25 +95,31 @@ public class MovePiece {
 //		System.out.println(row + " " + col);
 //		System.out.println(PrintBoard.board[row][col]);
 //		System.out.println(toCoordinates[0] + " " + toCoordinates[1]);
-		System.out.println(pieces.size());
+		int index = 0;
 		for (int i = 0; i < pieces.size(); i ++) {
-		
-			switch (PrintBoard.board[row][col]) {
-			case ANSI_TEXT+"wp"+ANSI_RESET: 
-				System.out.println("First works.");
-				System.out.println("to0:" + toCoordinates[0] + "from0:" + fromCoordinates[0] + "from1:" + fromCoordinates[1]);
-				System.out.println(row);
-				if (toCoordinates[0] == fromCoordinates[0] - 1) {
-					if (pieces.get(i).getTimesMoved() > 0) {
-						
-					}
-					System.out.println("It works. ");
-				}
-				break;
+			if (pieces.get(i).getLocation()[0] == fromCoordinates[0] && pieces.get(i).getLocation()[1] == fromCoordinates[1]) {
+				index = i;
 			}
 		}
 		
-		return true;
+		switch (PrintBoard.board[row][col]) {
+		case ANSI_TEXT+"wp"+ANSI_RESET: 
+//				System.out.println("First works.");
+//				System.out.println("to0:" + toCoordinates[0] + "to1:" + toCoordinates[1] + "from0:" + fromCoordinates[0] + "from1:" + fromCoordinates[1]);
+			if (toCoordinates[0] == fromCoordinates[0] - 2 && toCoordinates[1] == fromCoordinates[1]) {
+				if (pieces.get(index).getTimesMoved() > 0) {
+					System.out.println("\nYour pawn cannot move two spaces after the first move.");
+					return true;
+				}
+			}
+			if (toCoordinates[1] == fromCoordinates[1] - 1 && (toCoordinates[0] == fromCoordinates[0] - 1 || toCoordinates[0] == fromCoordinates[0] + 1) && PrintBoard.board[toCoordinates[0]][toCoordinates[1]].equals("  ")) {
+				System.out.println("\nYour pawn can only move diagonally when taking a black chess piece.");
+				return true;
+			}
+			break;
+		}
+		
+		return false;
 	}
 	
 	
